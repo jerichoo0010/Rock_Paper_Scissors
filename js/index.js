5c7c238dc4c025930f9c5a7bef9e9a6963f9883b
jikhub <57984423+jerichoo0010@users.noreply.github.com>
2022-11-26 22:01:17 +0800

    basic game done

--- js/index.js
+++ js/index.js
@@ -1,67 +1,127 @@
-const rock = "Rock",
-    paper = "Paper",
-    scissors = "Scissors";
+const rock = "rock",
+    paper = "paper",
+    scissors = "scissors";
+
+let computerChoice = ["rock", "paper", "scissors"]
+
+let currPlayerScore = 0, 
+    currComputerScore = 0,
+    compC
 
 let gameRes = document.getElementById("gameRes")
+let playerScore = document.getElementById("playerScore")
+let computerScore = document.getElementById("compScore")
 
-const playerSelection = paper;
-const computerSelection =  rock; // getComputerChoice();
+let playerSelection, computerSelection
+
+const num = Math.ceil(Math.random()*3)-1;
+// let r = (Math.random() + 1).toString(36).substring(7);
+
+
 
 
 function getComputerChoice(){
-
-
+    compC = computerChoice[num]
+    console.log("Computer choice:"+ compC);
+    return compC
 }
 
-playRound(playerSelection, computerSelection);
+for(let i=0; i<5; i++){
+    getComputerChoice();
+    playRound(playerSelection, compC);
+    
+    if(i == 4){
+        
+        if(currPlayerScore > currComputerScore){
 
+            gameRes.innerHTML = "Player wins! Congrats!"
+        }
+        else{
+            gameRes.innerHTML = "Computer wins! Congrats!"
+        }
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection, computerSelection)
+   // console.log(playerSelection, computerSelection)
+
+    playerSelection = prompt("Your pick?")
+
+    console.log("Player choice:"+ playerSelection)
+
+//    computerSelection =  paper; // getComputerChoice();
+
 
     if(playerSelection == rock && computerSelection == rock){
     
-        gameRes.innerHTML = "Tied!"
+        //gameRes.innerHTML = "Tied!"
+        console.log("Round draw!");
     }
     else if(playerSelection == rock && computerSelection == paper){
     
-        gameRes.innerHTML = "Sorry. You lost!"
+      //  gameRes.innerHTML = "Computer wins for this round!"
+        currComputerScore+= 1;
+        console.log("Current Computer Score:"+ currComputerScore)
+        console.log("Computer wins for this round!")
+        computerScore.innerHTML = currComputerScore
+        
     }
     else if(playerSelection == rock && computerSelection == scissors){
     
-        gameRes.innerHTML = "Congrats! You defeated the computer!"
+       // gameRes.innerHTML = "Congrats! You defeated the computer!"
+        currPlayerScore+= 1;
+        console.log("Current Player Score:"+ currPlayerScore)
+        console.log("Player wins for this round!")
+        playerScore.innerHTML = currPlayerScore
     }
 
 
 
     else if(playerSelection == paper && computerSelection == paper){
     
-        gameRes.innerHTML = "Tied!"
+      //  gameRes.innerHTML = "Tied!"
+        console.log("Round draw!");
     }
     else if(playerSelection == paper && computerSelection == rock){
     
-        gameRes.innerHTML = "Congrats! You defeated the computer!"
+  //      gameRes.innerHTML = "Congrats! You defeated the computer!"
+        currPlayerScore+= 1;
+        console.log("Current Player Score:"+ currPlayerScore)
+        console.log("Player wins for this round!")
+        playerScore.innerHTML = currPlayerScore
+        
     }
     else if(playerSelection == paper && computerSelection == scissors){
     
-        gameRes.innerHTML = "Sorry. You lost!"
+    //    gameRes.innerHTML = "Sorry. You lost!"
+        console.log("Computer wins for this round!")
+        currComputerScore+= 1;
+        console.log("Current Computer Score:"+ currComputerScore)
+        computerScore.innerHTML = currComputerScore
     }
  
 
     else if(playerSelection == scissors && computerSelection == scissors){
     
-        gameRes.innerHTML = "Tied!"
+     //   gameRes.innerHTML = "Tied!"
+        console.log("Round draw!");
     }
     else if(playerSelection == scissors && computerSelection == paper){
     
-        gameRes.innerHTML = "Congrats! You defeated the computer!"
+  //      gameRes.innerHTML = "Congrats! You defeated the computer!"
+        currPlayerScore+= 1;
+        console.log("Current Player Score:"+ currPlayerScore)
+        console.log("Player wins for this round!")
+        playerScore.innerHTML = currPlayerScore
     }
     else if(playerSelection == scissors && computerSelection == rock){
     
-        gameRes.innerHTML = "Sorry. You lost!"
+   //     gameRes.innerHTML = "Sorry. You lost!"
+        console.log("Computer wins for this round!")
     }
     else{
-        gameRes.innerHTML = "Invalid Input!"
+     //   gameRes.innerHTML = "Invalid Input!"
+        console.log("Round draw!");
     }
 
 
